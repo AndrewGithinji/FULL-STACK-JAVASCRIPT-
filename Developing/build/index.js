bf62ca3fdfbd11c397ca937a15c614d8038ca75d
AndrewGithinji <andrewgithinji@hotmail.com>
2022-12-19 18:14:40 +0000

    Developing with TypeScript

---
+++ Developing/build/index.js
@@ -0,0 +1,5 @@
+"use strict";
+// convert the file into typescript//
+var myName = "your name";
+var hello = function (userName) { return "hello, ".concat(userName); };
+console.log(hello(myName));
---
+++ Developing/src/index.ts
@@ -0,0 +1,7 @@
+// convert the file into typescript//
+
+const myName = "your name";
+
+const hello = (userName: string): string => `hello, ${userName}`;
+
+console.log(hello(myName));
